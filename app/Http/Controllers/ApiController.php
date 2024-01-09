91e4e794027ca7a3c689afb7232ad532409c4f00
strayneko <bayuredn1i0@gmail.com>
2024-01-09 10:23:00 +0700

    Feat: add simple post api for testing

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,17 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Movie;
+use App\Models\User;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+        $data = $request->input('data');
+
+        try{
+            $mappedData = $this->mapData($data);
+            User::insert($mappedData);
+
         return response()->json([
-            'data' => Movie::all(),
+            'status'  => Response::HTTP_CREATED,
+            'message' => '',
         ]);
+        }catch(QueryException $e){
+            Log::error($e->getMessage());
+
+            return response()->json([
+                'status'  => Response::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function mapData(array $data): array
+    {
+        return array_map(function($item){
+            return [
+              'email'       => $item['email'] ?? null,
+              'name'        => $item['password'] ?? null,
+              'created_at' => now(),
+              'updated_at' => now(),
+            ];
+        }, $data);
     }
 
     public function store()
