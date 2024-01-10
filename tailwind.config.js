ee442c727b4d6a54eb916cbc985385a69bac39e4
strayneko <bayuredn1i0@gmail.com>
2024-01-10 08:51:01 +0700

    Feat: add safelist class to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ export default {
         './resources/views/**/*.blade.php',
         "./node_modules/flowbite/**/*.js"
     ],
-
+    safelist: [
+        'max-w-2xl',
+    ],
     theme: {
         extend: {
             fontFamily: {
